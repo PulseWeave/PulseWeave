1dc124326021d45f0258acdf0f9b0aedf9abd64b
EagleFandel <2170257193@qq.com>
2025-08-31 00:44:44 +0800

    feat: initial commit with SDK import
    - Imported the necessary SDK for PulseWeave project.
    - Set up the initial project structure.

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        maven { url = uri("https://maven.rokid.com/repository/maven-public/") }
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PulseWeave"
+include(":app")
